2a0f3793217b2313064540fc0ad2c179c2d1ab74
dashanji <952129620@qq.com>
2020-10-20 21:32:19 +0800

    pmm alloc/free

--- build/boot/CMakeFiles/bootsector.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bootsector.dir/bootsector.S.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang ASM)
-  include(CMakeFiles/bootsector.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/kernel/asm/CMakeFiles/asm.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/asm.dir/asm.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/asm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
